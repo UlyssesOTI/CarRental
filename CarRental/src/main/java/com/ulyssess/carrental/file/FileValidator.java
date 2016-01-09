30f6ce4fc72839b71ccb78de0610355ed085ec79
Ulysses <Ulysses@UlyssesPC>
2016-01-09 16:44:18 +0200

    last

--- CarRental/src/main/java/com/ulyssess/carrental/file/FileValidator.java
+++ CarRental/src/main/java/com/ulyssess/carrental/file/FileValidator.java
@@ -1,10 +1,11 @@
 package com.ulyssess.carrental.file;
 
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 
-
+@Component
 public class FileValidator implements Validator {
 	public boolean supports(Class<?> paramClass) {
 		return FileClass.class.equals(paramClass);
